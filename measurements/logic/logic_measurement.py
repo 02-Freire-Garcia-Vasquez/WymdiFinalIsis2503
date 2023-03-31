b221e4025b0fcd943ce082dc5e02d055010ad487
samuelfreire03 <s.freire@uniandes.edu.co>
2023-03-30 21:26:06 -0500

    holameasurements

--- measurements/logic/logic_measurement.py
+++ measurements/logic/logic_measurement.py
@@ -1,7 +1,7 @@
 from ..models import Measurement
 
 def get_measurements():
-    queryset = Measurement.objects.all().order_by('-dateTime')[:10]
+    queryset = Measurement.objects.all().order_by('paciente')
     return (queryset)
 
 def create_measurement(form):
@@ -9,11 +9,13 @@ def create_measurement(form):
     measurement.save()
     return ()
 
-def create_measurement_object(variable_id, value, unit, place):
+def create_measurement_object(variable_id, fecha, lugar, tipo,motivo,enfer):
     measurement = Measurement()
-    measurement.variable = variable_id
-    measurement.value = value
-    measurement.unit = unit
-    measurement.place = place
+    measurement.paciente = variable_id
+    measurement.fechaConsulta = fecha
+    measurement.lugarConsulta = lugar
+    measurement.tipoConsulta = tipo
+    measurement.motivoConsulta = motivo
+    measurement.enfermedad = enfer
     measurement.save()
     return ()
