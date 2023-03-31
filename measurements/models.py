ec96bfa8157e72784bf76cf9c94ce6e5ca494ed0
samuelfreire03 <s.freire@uniandes.edu.co>
2023-03-30 21:31:44 -0500

    cambios

--- measurements/models.py
+++ measurements/models.py
@@ -7,7 +7,7 @@ class Measurement(models.Model):
     lugarConsulta = models.CharField(max_length=50, default=None)
     tipoConsulta = models.CharField(max_length=50, default=None)
     motivoConsulta = models.CharField(max_length=50, default=None)
-    enfermedad = models.CharField(max_length=50, default=None)
+    enfermedad = models.CharField(max_length=50, default="")
 
     def __str__(self):
-        return '%s %s' % (self.value, self.unit)
+        return '%s %s' % (self.fechaConsulta, self.lugarConsulta)
