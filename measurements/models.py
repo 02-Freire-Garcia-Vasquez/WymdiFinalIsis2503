595d8efdcaca155f00590d0432f3dd5f62f025a0
samuelfreire03 <s.freire@uniandes.edu.co>
2023-03-30 21:29:42 -0500

    cambios

--- measurements/models.py
+++ measurements/models.py
@@ -3,11 +3,11 @@ from variables.models import Variable
 
 class Measurement(models.Model):
     paciente = models.ForeignKey(Variable, on_delete=models.CASCADE, default=None)
-    fechaConsulta = models.CharField(max_length=50)
-    lugarConsulta = models.CharField(max_length=50)
-    tipoConsulta = models.CharField(max_length=50)
-    motivoConsulta = models.CharField(max_length=50)
-    enfermedad = models.CharField(max_length=50)
+    fechaConsulta = models.CharField(max_length=50, default=None)
+    lugarConsulta = models.CharField(max_length=50, default=None)
+    tipoConsulta = models.CharField(max_length=50, default=None)
+    motivoConsulta = models.CharField(max_length=50, default=None)
+    enfermedad = models.CharField(max_length=50, default=None)
 
     def __str__(self):
         return '%s %s' % (self.value, self.unit)
